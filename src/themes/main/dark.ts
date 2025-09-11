afeb9b7618e57c4ccf5a32b85783b4a25e1726c0
Willbard Beatus Mloka <130184031+willbardDev@users.noreply.github.com>
2025-09-11 12:46:34 +0300

    fix screen size

--- src/themes/main/dark.ts
+++ src/themes/main/dark.ts
@@ -31,10 +31,10 @@ export const mainTheme: ThemeOptions = {
   breakpoints: {
     values: {
       xs: 0,
-      sm: 576,
-      md: 768,
-      lg: 992,
-      xl: 1200,
+      sm: 600,
+      md: 800,
+      lg: 1280,
+      xl: 1920,
     },
   },
   palette: {
