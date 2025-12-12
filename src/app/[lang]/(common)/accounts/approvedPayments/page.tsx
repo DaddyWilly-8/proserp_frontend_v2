c375249fa09ea58f8d1455e62b58cc184df86807
Willbard Beatus Mloka <130184031+willbardDev@users.noreply.github.com>
2025-12-12 16:53:44 +0300

    solve build error v10

--- src/app/[lang]/(common)/accounts/approvedPayments/page.tsx
+++ src/app/[lang]/(common)/accounts/approvedPayments/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 export const dynamic = "force-dynamic";
 import ApprovedPayments from '@/components/processApproval/approvedRequisitions/ApprovedPayments'
 import React from 'react'
