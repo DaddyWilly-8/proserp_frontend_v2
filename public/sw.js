600d9ae41d446fe4a4a7889f97cacf12c1dceb22
Willbard Beatus Mloka <130184031+willbardDev@users.noreply.github.com>
2026-08-10 06:07:54 +0300

    Brother works

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // CACHE VERSIONING AND UPDATE LOGIC
-const CACHE_VERSION = 'v1786285045553'; // Increment this to force update
+const CACHE_VERSION = 'v1786330947606'; // Increment this to force update
 self.addEventListener('install', event => {
 	self.skipWaiting();
 });
