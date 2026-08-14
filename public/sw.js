3a5f09970e8456e5c63268a4717a2922a52a5ffd
Willbard Beatus Mloka <130184031+willbardDev@users.noreply.github.com>
2026-08-14 10:37:50 +0300

    eliya updates in human resource

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // CACHE VERSIONING AND UPDATE LOGIC
-const CACHE_VERSION = 'v1786661500142'; // Increment this to force update
+const CACHE_VERSION = 'v1786691213539'; // Increment this to force update
 self.addEventListener('install', event => {
 	self.skipWaiting();
 });
