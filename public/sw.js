2cb7bbb8e278ab76d416aabfa0ac555752749134
Willbard Beatus Mloka <130184031+willbardDev@users.noreply.github.com>
2026-08-17 08:40:00 +0300

    eeliya changes

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // CACHE VERSIONING AND UPDATE LOGIC
-const CACHE_VERSION = 'v1786790759999'; // Increment this to force update
+const CACHE_VERSION = 'v1786944913079'; // Increment this to force update
 self.addEventListener('install', event => {
 	self.skipWaiting();
 });
