980453dbc8daf37868aad0068086d3555ab11db7
Willbard Beatus Mloka <130184031+willbardDev@users.noreply.github.com>
2026-08-19 10:38:15 +0300

    symbol error in rq onscreen

--- src/components/processApproval/RequisitionsOnScreen.tsx
+++ src/components/processApproval/RequisitionsOnScreen.tsx
@@ -419,7 +419,7 @@ const RequisitionsOnScreen: React.FC<Props> = ({
                               align='right'
                               sx={{ fontFamily: 'monospace' }}
                             >
-                              {`${item.quantity?.toLocaleString()} ${item.measurement_unit.symbol}`}
+                              {`${item.quantity?.toLocaleString()} ${item.measurement_unit?.symbol}`}
                             </TableCell>
                             <TableCell
                               colSpan={1}
