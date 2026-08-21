a2c243cd987460a28bda8b301dae14880376d2c8
Willbard Beatus Mloka <130184031+willbardDev@users.noreply.github.com>
2026-08-21 18:29:11 +0300

    changes

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // CACHE VERSIONING AND UPDATE LOGIC
-const CACHE_VERSION = 'v1787293030169'; // Increment this to force update
+const CACHE_VERSION = 'v1787325908975'; // Increment this to force update
 self.addEventListener('install', event => {
 	self.skipWaiting();
 });
